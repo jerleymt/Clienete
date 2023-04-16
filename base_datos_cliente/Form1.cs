629acc33afb96bb8d8edf238da013096957f2efc
Jose Erley <jerley@live.com>
2023-04-16 13:44:34 -0500

    Se agrego el formulario

---
+++ base_datos_cliente/Form1.cs
@@ -0,0 +1,10 @@
+namespace base_datos_cliente
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ capa_entidad_cliente/Form1.cs
@@ -0,0 +1,10 @@
+namespace capa_entidad_cliente
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
