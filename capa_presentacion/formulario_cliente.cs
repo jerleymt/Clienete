58bd22b05d8590bb96917b74dcb924ae0e5c6b92
Jose Erley <jerley@live.com>
2023-04-16 13:52:45 -0500

    le puse nombre a los objetos del formulario

--- capa_presentacion/formulario_cliente.cs
+++ capa_presentacion/formulario_cliente.cs
@@ -1,8 +1,8 @@
 namespace capa_presentacion
 {
-    public partial class Form1 : Form
+    public partial class formulario_cliente : Form
     {
-        public Form1()
+        public formulario_cliente()
         {
             InitializeComponent();
         }
